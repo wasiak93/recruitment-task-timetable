bc248a721dcf649be5f34312fd8186e1af159a9c
Kacper Wasiak <k.wasiak@nowaera.pl>
2024-11-29 12:04:27 +0100

    change in search input

--- src/components/StopsSearch.vue
+++ src/components/StopsSearch.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import { ref, defineEmits, defineProps } from "vue";
+import { defineEmits, defineProps } from "vue";
 
 defineProps<{
   modelValue: string;
 }>();
-
-const isFocused = ref(false);
 
 defineEmits<{
   (e: "update:modelValue", value: string): void;
@@ -13,70 +11,71 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="input-group search mx-2" :class="{ 'search--border': isFocused }">
-    <input
-      type="text"
-      class="form-control px-3 py-2 search__input"
-      :class="{ 'search__input--not-border': !isFocused }"
-      :value="modelValue"
-      placeholder="Search..."
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-      "
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-      data-test="filter-input"
-    />
-    <span v-if="isFocused" class="search__input-absolute-element px-1"
-      >Search</span
-    >
-    <span class="input-group-text bg-white" v-if="!isFocused">
-      <img
-        src="/icons/search.svg"
-        alt="Search Icon"
-        width="16"
-        height="16"
-        class="d-inline-block"
-        data-test="search-img"
+  <div class="input-group search mx-2">
+    <div class="form-group">
+      <input
+        type="text"
+        id="name rounded"
+        placeholder="Name"
+        class="px-3 py-2"
+        :value="modelValue"
+        @input="
+          $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+        "
+        data-test="filter-input"
       />
-    </span>
+      <label for="name">Search</label>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.form-control:focus {
-  outline: none;
-  border-color: #007bff;
-}
+.form-group {
+  --pad: 0.5rem;
+  --pad-left: 1rem;
+  position: relative;
 
-.search {
-  width: 288px;
-  &__input {
+  input {
+    border-radius: 5px;
+    border: 1px solid #e2e4ea;
     font-size: 12px;
+    line-height: 22px;
     color: #1a1a1a;
+    width: 288px;
+    background-image: url("../../public/icons/search.svg");
+    background-repeat: no-repeat;
+    background-position: right 2% top 50%;
 
-    &-absolute-element {
-      position: absolute;
-      z-index: 999;
-      top: 0;
-      left: 14px;
-      font-size: 10px;
-      transform: translateY(-50%);
-      background-color: white;
-      color: #63666e;
+    &::placeholder {
+      opacity: 0;
     }
 
-    &--not-border {
-      border-right: none;
+    &:focus,
+    &:not(:placeholder-shown) {
+      background-image: none;
+
+      & + label {
+        top: 0;
+        transform: translateY(-50%) scale(0.8);
+        font-size: 10px;
+        background-color: white;
+      }
+    }
+
+    &:focus-visible {
+      outline-color: #007bff;
     }
   }
 
-  &--border {
-    &.input-group:not(.has-validation)
-      > :not(:last-child):not(.dropdown-toggle):not(.dropdown-menu):not(.form-floating) {
-      border-top-right-radius: 0.375rem;
-      border-bottom-right-radius: 0.375rem;
-    }
+  label {
+    position: absolute;
+    left: var(--pad-left);
+    top: 50%;
+    padding: 2px;
+    transform: translateY(-50%);
+    pointer-events: none;
+    font-size: 12px;
+    color: #63666e;
   }
 }
 </style>
